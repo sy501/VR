24b129ba3c1d3d11051e5460496f116da1bac39b
sy501 <sydoris990501@gmail.com>
2025-11-28 11:56:57 +0800

    vr

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "首頁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1760462565978962,
+        "pitch": -0.23418252780965076,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.0927109083351034,
+          "pitch": -0.4158766680258523,
+          "title": "松山文創園區",
+          "text": "已被訂為市定古蹟<div>現已成為台北東區重要的公共藝文建設</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
